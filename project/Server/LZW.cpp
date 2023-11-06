3942e61d76a788f1a96c77100276f9cfcab31723
Chihan-Zheng <chihan@seas.upenn.edu>
2023-11-06 00:01:46 -0500

    Some updates to baseline and LZW

--- project/Server/LZW.cpp
+++ project/Server/LZW.cpp
@@ -9,8 +9,8 @@
     send_data: the output data: 32bits header + compressed data
 */
 uint16_t LZW (char *in, uint16_t in_length, uint16_t *send_data){
-    uint16_t dict[MAX_DICT_SIZE][256];         //SIZE may be extended in the for loop
-    uint16_t store_array[in_length];
+    uint16_t dict[MAX_DICT_SIZE][256] = {0};         //SIZE may be extended in the for loop
+    uint16_t store_array[in_length] {0};
     uint16_t code = (uint16_t)*in;    //current code
     uint16_t next_code = 256;         //next new code
     uint8_t next_char = ' ';          //next char of in
@@ -18,40 +18,60 @@ uint16_t LZW (char *in, uint16_t in_length, uint16_t *send_data){
     uint16_t j = 0;                   //index of store array (should j++ every time after store)
     unsigned char shift = 0;
 
-    std::cout << "\ninput chunk length: " << in_length << std::endl;
+    std::cout << "input chunk length: " << in_length <<std::endl;
     for (int i = 0; i < (in_length - 1); i++){
-        next_char = *(in + 1);       //next character
-        if (temp_code = dict[code][next_char]){     //check strings exist or not
+        next_char = *(in + i + 1);       //next character
+        if ((temp_code = dict[code][next_char]) != 0){     //check strings exist or not
             code = temp_code;                       //if exists, then go to the code to match next character
         }else{
-            if (j == 0){
+            if (j == 0){   //the first code
                 shift = 3;
                 store_array[0] = code << shift;
-                std::cout << "code: " << next_code << std::endl;
+                // std::cout << "code: " << next_code << std::endl;
                 dict[code][next_char] = next_code++;     //encode new code
                 code = next_char;       //restart from the next_character
                 j++;
-            }else{
-                shift = shift + 3;
-                store_array[j] = code << shift;
-                shift = 16 - shift;
-                store_array[j-1] = store_array[j-1] | (code >> shift);
-                shift = 16 - shift;
-                store_array[j-1] = swap_endian_16(store_array[j-1]);
-                j++;
-                 
+            }else{       
+                if (shift < 13){      //check whether empty space of store_array[j-1] is greater than 13
+                    shift = shift + 3;
+                    store_array[j] = code << shift;     //xxxxx00 yyyyyyyy -> xxxxx|yy yyyyyy00 (shift here is 2)
+                    shift = 16 - shift;      
+                    store_array[j-1] = store_array[j-1] | (code >> shift);
+                    shift = 16 - shift;
+                    store_array[j-1] = swap_endian_16(store_array[j-1]);
+                    j++;
+                }else{
+                    char vacant_bit_number = shift - 13;      //the rest bits number after code is written to store_array[j-1]
+                    store_array[j-1] = store_array[j-1] | (code << vacant_bit_number);
+                    shift = vacant_bit_number;     
+                    //do not j++ here 
+                }
+                // printf("shift: %d\n", shift);
+                // std::cout << "code: " << next_code << std::endl;
                 dict[code][next_char] = next_code++;     //encode new code
-                std::cout << "code: " << next_code << std::endl;
                 code = next_char;       //restart from the next_character
             }
         }
-        in++;
     }
-    shift = shift + 3;
-    store_array[j] = code << shift;
-    shift = 16 - shift;
-    store_array[j-1] = store_array[j-1] | (code >> shift);        //store the last part of in
-    store_array[j-1] = swap_endian_16(store_array[j-1]);
+    //deal with the last part of chunk or if there is only one character in chunk
+    if (in_length == 1) {      //if chunk length is 1: a single character
+        shift = shift + 3;
+        store_array[j] = code << shift;
+        store_array[j] = swap_endian_16(store_array[j]);
+    }else{ 
+        if (shift < 13){ 
+            shift = shift + 3;
+            store_array[j] = code << shift;
+            store_array[j] = swap_endian_16(store_array[j]);
+            shift = 16 - shift;
+            store_array[j-1] = store_array[j-1] | (code >> shift);        //store the last part of in
+        }else{
+            char vacant_bit_number = shift - 13;
+            store_array[j-1] = store_array[j-1] | (code << vacant_bit_number);
+            j = j - 1;
+        }
+        store_array[j-1] = swap_endian_16(store_array[j-1]);
+    }
 
     //-----------------------generate LZW output----------------------------
     uint32_t header = 0;
@@ -60,7 +80,7 @@ uint16_t LZW (char *in, uint16_t in_length, uint16_t *send_data){
     header = compressed_length << 1;
     header = swap_endian_32(header);
     memcpy(send_data, &header, 4);
-    memcpy(send_data + 4, store_array, compressed_length);
+    memcpy(send_data + 2, store_array, compressed_length);
     //-----------------------------------------------------------------------------
     return (compressed_length + 4);
     
--- project/Server/baseline.cpp
+++ project/Server/baseline.cpp
@@ -37,8 +37,8 @@ int main()
     std::cout << "chunk number: " << boundary_num << std::endl;
 	for (int i = 0; i < boundary_num; i++){
         deDup_header = deDup(ArrayOfChunks[i], chunk_size[i], chunkTable);
-        deDup_header = swap_endian_32(deDup_header);
         if (deDup_header & 1u){
+            deDup_header = swap_endian_32(deDup_header);
             std::cout << "deDup_header - boundary: " << i << std::endl;
             if (fwrite(&deDup_header, 1, sizeof(deDup_header), File) != sizeof(deDup_header))
                 Exit_with_error("fwrite dedup header to compressed_data.bin failed");
@@ -46,10 +46,13 @@ int main()
             // arr_offset += sizeof(deDup_header) / 2;
         }else{
             std::cout << "LZW_header - boundary: " << i << std::endl;
-            int in_length = chunk_size[i];
+            uint16_t in_length = chunk_size[i];
             LZW_output_length = LZW(ArrayOfChunks[i], in_length, LZW_send_data);
+            std::cout << "LZW_output_length[" << i << "]: " << LZW_output_length << "\n" <<std::endl;
             if (fwrite(LZW_send_data, 1, LZW_output_length, File) != LZW_output_length)
                 Exit_with_error("fwrite LZW output to compressed_data.bin failed");
+            memset(LZW_send_data, 0, (Max_Chunk_Size + 2) * sizeof(uint16_t));
+
             // memcpy(send_data_arr_total + arr_offset, LZW_send_data, LZW_output_length);
             // arr_offset += LZW_output_length / 2;
         }
@@ -57,7 +60,55 @@ int main()
 
     if (fclose(File) != 0)
     Exit_with_error("fclose for send_data failed");
+  
 
+
+
+
+//-------------------------------- for testing one character: a(LZW) ---------------------------------
+/*     FILE* fp = fopen("LZW_test.txt","r" );
+	if(fp == NULL ){
+		perror("fopen error");
+		return 0;
+	}
+
+	fseek(fp, 0, SEEK_END); // seek to end of file
+	int file_size = ftell(fp); // get current file pointer
+	fseek(fp, 0, SEEK_SET); // seek back to beginning of file
+
+	char* buff = (char *)malloc((sizeof(unsigned char) * file_size ));	
+	if(buff == NULL)
+	{
+		perror("not enough space");
+		fclose(fp);
+		return 0;
+	}
+
+	int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
+ 
+    uint16_t *LZW_send_data = (uint16_t *)calloc(Max_Chunk_Size + 2, sizeof(uint16_t));
+    uint16_t in_length = file_size;
+    uint16_t LZW_output_length;  
+
+
+    FILE *File = fopen("./compressed_data.bin", "wb");
+    if (File == NULL)
+        Exit_with_error("fopen for send_data failed");
+
+    LZW_output_length = LZW(buff, in_length, LZW_send_data);
+    std::cout << "input buff: " << buff << std::endl;
+    std::cout << "compressed length: " << LZW_output_length << std::endl;
+
+    for (int i = 0; i < LZW_output_length / 2; i++){
+        std::cout << "send_data[" << i << "]: "<< LZW_send_data[i] << std::endl;
+    }
+
+    if (fwrite(LZW_send_data, 1, LZW_output_length, File) != LZW_output_length)
+        Exit_with_error("fwrite LZW output to compressed_data.bin failed");
+
+    if (fclose(File) != 0)
+        Exit_with_error("fclose for send_data failed"); */
+//------------------------------------------ end of test --------------------------------------------------
 
 
 	return 0;
