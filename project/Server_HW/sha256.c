4165a56ca9ef3dbc274da75ebfda2600db77d8c0
sandhuam <42740388+sandhuam@users.noreply.github.com>
2023-11-15 21:13:05 -0500

    Add files via upload

--- project/Server_HW/sha256.c
+++ project/Server_HW/sha256.c
@@ -1,9 +1,16 @@
-#include "Constants.h"
-#include "stopwatch.h"
-#include <iomanip>
-#include <sstream>
-#include <vector>
-using namespace std;
+/*********************************************************************
+* Filename:   sha256.c
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Implementation of the SHA-256 hashing algorithm.
+              SHA-256 is one of the three algorithms in the SHA2
+              specification. The others, SHA-384 and SHA-512, are not
+              offered in this implementation.
+              Algorithm specification can be found here:
+               * http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
+              This implementation uses little endian byte order.
+*********************************************************************/
 
 /*************************** HEADER FILES ***************************/
 #include <stdlib.h>
@@ -167,42 +174,4 @@ void sha256_hash_file(SHA256_CTX* ctx, const BYTE data[], BYTE hash[], size_t da
 	sha256_init(ctx);
 	sha256_update(ctx, data, dataLen);
     sha256_final(ctx, hash);
-}
-
-
-string hexconvert(unsigned char* bytes, int size) {
-    stringstream ss;
-    ss << hex << setfill('0');
-    for (int i = 0; i < size; i++) {
-        ss << setw(2) << static_cast<int>(bytes[i]);
-    }
-    return ss.str();
-}
-
-uint32_t deDup(char* inputChunk, uint16_t chunk_size,
-    std::unordered_map<string, uint32_t>& chunkTable,
-    stopwatch& stopwatch) {
-    unsigned char uinputChunk[static_cast<int>(chunk_size)];
-    for (size_t i = 0; i < chunk_size; i++) {
-            uinputChunk[i] = static_cast<unsigned char>(inputChunk[i]);
-        }
-    
-        //size_t length = sizeof(inputChunk)/sizeof(char);
-        //size_t length = chunk_size;
-    BYTE buf[SHA256_BLOCK_SIZE];
-    SHA256_CTX ctx;
-        stopwatch.start();
-    sha256_hash(&ctx, uinputChunk, buf, 1);
-    string hash = hexconvert(buf, SHA256_BLOCK_SIZE);
-        stopwatch.stop();
-        auto it = chunkTable.find(hash);
-
-        if (it == chunkTable.end()) {
-            uint32_t newIndex = static_cast<uint32_t>(chunkTable.size());
-            chunkTable[hash] = newIndex;
-            return (newIndex << 1);
-        } else {
-            return (it->second << 1) | 1u;
-        }
-
 }
