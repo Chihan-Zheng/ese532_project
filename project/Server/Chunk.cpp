5e2dc65c9d76125120b5a2c508b23b4ec05486d0
sandhuam <42740388+sandhuam@users.noreply.github.com>
2023-11-30 09:19:53 -0500

    Add files via upload

--- project/Server/Chunk.cpp
+++ project/Server/Chunk.cpp
@@ -28,67 +28,68 @@ uint64_t hash_func(unsigned char *input, unsigned int pos)
 
 //Calculating rolling hash for the input. Once the hash at some point modulus to some number equals
 //target 0, or it hits the maximum chunk size, we call it a boundary.
-int cdc_window(unsigned char *buff, unsigned int buff_size, char** chunk, uint16_t *chunk_size)
+void cdc_window(unsigned char *buff, unsigned int buff_size, char* chunk, uint16_t chunk_size, int *offset_buff, int *pipeline_drained)
 {
-	uint64_t *hash = (uint64_t *)malloc(sizeof(uint64_t) * (buff_size - WIN_SIZE));
-	int boundary_index = 0;
-	int previous_boundary = 0;
+    unsigned char *buff_new = buff + &offset_buff;
+    unsigned int buff_size_new = buff_size - &offset_buff;
+    
+	uint64_t *hash = (uint64_t *)malloc(sizeof(uint64_t) * (buff_size_new - WIN_SIZE));
+    
 	std::cout << "---------------------------------Boundary Index--------------------------------------" << std::endl;
-    for(unsigned int i = WIN_SIZE; i<buff_size - WIN_SIZE; i++){
+    for(unsigned int i = WIN_SIZE; i<buff_size_new - WIN_SIZE; i++){
 		if(i == WIN_SIZE)
-		hash[i] = hash_func(buff, WIN_SIZE);
+		hash[i] = hash_func(buff_new, WIN_SIZE);
 		else
-		hash[i] = hash[i-1] * PRIME - buff[i-1]*pow(PRIME, WIN_SIZE+1) + buff[i+WIN_SIZE-1]*PRIME;
-		if((((hash[i] % MODULUS) == TARGET)&&(i-previous_boundary>=MIN_CHUNK))||(i-previous_boundary>=MAX_CHUNK)||(i==buff_size-WIN_SIZE-1)) {
-		// if((((hash[i] % MODULUS) == TARGET)&&(i-previous_boundary>=MIN_CHUNK))||(i-previous_boundary>=MAX_CHUNK)||(i==buff_size-WIN_SIZE)) {
+		hash[i] = hash[i-1] * PRIME - buff_new[i-1]*pow(PRIME, WIN_SIZE+1) + buff_new[i+WIN_SIZE-1]*PRIME;
+		if((((hash[i] % MODULUS) == TARGET)&&(i>=MIN_CHUNK))||(i>=MAX_CHUNK)||(i==buff_size_new-WIN_SIZE-1)) {
+		// if((((hash[i] % MODULUS) == TARGET)&&(i-previous_boundary>=MIN_CHUNK))||(i-previous_boundary>=MAX_CHUNK)||(i==buff_size_new-WIN_SIZE)) {
 
 			printf("The index %d is a boundary\n", i); //Print out the boundary we found.
-			chunk[boundary_index] = (char*)malloc(sizeof(char)*MAX_CHUNK);
-			if(i<buff_size-WIN_SIZE-1){
-				memcpy(chunk[boundary_index], buff+previous_boundary, i-previous_boundary);
-				chunk_size[boundary_index] = i - previous_boundary;
+			chunk = (char*)malloc(sizeof(char)*MAX_CHUNK);
+			if(i<buff_size_new-WIN_SIZE-1){
+				memcpy(chunk, buff_new, i);
+				chunk_size = i;
 			}else{
-				memcpy(chunk[boundary_index], buff+previous_boundary, buff_size-previous_boundary);  //because the hash cannot calculate after buff_size-win_size, the last chunk copy will be different.
-				chunk_size[boundary_index] = i - previous_boundary + WIN_SIZE + 1;
+				memcpy(chunk, buff_new, buff_size_new);  //because the hash cannot calculate after buff_size_new-win_size, the last chunk copy will be different.
+				chunk_size = i + WIN_SIZE + 1;
+                &pipeline_drained = 1;
 			}
-			previous_boundary = i;
-            boundary_index++;
+            
+            &offset_buff += i;
 			//printf("The hash calculated at this index is %d\n",hash[i]); //Print out the hash value calculated at this char.
 			// printf("The calculated 8 bytes are: %c%c%c%c%c%c%c%c\n",buff[i],buff[i+1],buff[i+2],buff[i+3],buff[i+4],buff[i+5],buff[i+6],buff[i+7]); //print out the 8 characters that hash based on.
         }
 		
 	}
 	free(hash);
-	
-	return boundary_index;
-
 }
 //read the input file and call the rolling hash function.
-int cdc( const char* file, char** chunk, uint16_t *chunk_size)
+// int cdc( const char* file, char** chunk, uint16_t *chunk_size)
+int cdc( unsigned char* buff, int buff_size,char** chunk, uint16_t *chunk_size)
 {
-	FILE* fp = fopen(file,"r" );
-	if(fp == NULL ){
-		perror("fopen error");
-		return 0;
-	}
+	// FILE* fp = fopen(file,"r" );
+	// if(fp == NULL ){
+	// 	perror("fopen error");
+	// 	return 0;
+	// }
 
-	fseek(fp, 0, SEEK_END); // seek to end of file
-	int file_size = ftell(fp); // get current file pointer
-	fseek(fp, 0, SEEK_SET); // seek back to beginning of file
+	// fseek(fp, 0, SEEK_END); // seek to end of file
+	// int file_size = ftell(fp); // get current file pointer
+	// fseek(fp, 0, SEEK_SET); // seek back to beginning of file
 
-	unsigned char* buff = (unsigned char *)malloc((sizeof(unsigned char) * file_size ));	
-	if(buff == NULL)
-	{
-		perror("not enough space");
-		fclose(fp);
-		return 0;
-	}
+	// unsigned char* buff = (unsigned char *)malloc((sizeof(unsigned char) * file_size ));	
+	// if(buff == NULL)
+	// {
+	// 	perror("not enough space");
+	// 	fclose(fp);
+	// 	return 0;
+	// }
 
-	int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
+	// int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
 	
-	int boundary_num = cdc_window(buff, file_size, chunk, chunk_size);
+	int boundary_num = cdc_window(buff,  buff_size, chunk, chunk_size);
 
-    free(buff);
+    // free(buff);
     return boundary_num;
 }
 void test_print_chunk(char** chunk, int boundary_num){
@@ -101,8 +102,9 @@ void test_print_chunk(char** chunk, int boundary_num){
 //main function for testing. Shouldn't be included in the final interface.
 /* int main()
 {
+	uint16_t chunk_size;
     char *ArrayOfChunks[MAX_BOUNDARY];
-	int boundary_num = cdc("LittlePrince.txt", ArrayOfChunks);
+	int boundary_num = cdc("test.txt", ArrayOfChunks, &chunk_size);
 	test_print_chunk(ArrayOfChunks, boundary_num);
 	return 0;
 } */
